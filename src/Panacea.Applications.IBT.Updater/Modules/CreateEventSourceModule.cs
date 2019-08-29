00c1952ab73ff665c62c594f79012dbf3e9ca16b
Portno <j.remoundos@dotbydot.gr>
2019-08-29 11:58:51 +0300

    event logs

---
+++ src/Panacea.Applications.IBT.Updater/Modules/CreateEventSourceModule.cs
@@ -0,0 +1,39 @@
+﻿using IBT.Updater.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBT.Updater.Modules
+{
+    [Interfaces.Action("Logging")]
+
+    class CreateEventSourceModule : Module
+    {
+        internal override Task<bool> OnPreUpdate(SafeDictionary<string, object> keys)
+        {
+            CreateEventSourceSample1();
+            return base.OnPreUpdate(keys);
+        }
+        static void CreateEventSourceSample1()
+        {
+            string myLogName;
+            string sourceName = "Panacea";
+
+            // Create the event source if it does not exist.
+            if (!EventLog.SourceExists(sourceName))
+            {
+                // Create a new event source for the custom event log
+                // named "myNewLog."  
+
+                myLogName = "Application";
+                EventSourceCreationData mySourceData = new EventSourceCreationData(sourceName, myLogName);
+                EventLog.CreateEventSource(mySourceData);
+            }
+
+        }
+    }
+}
